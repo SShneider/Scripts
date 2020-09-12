3e51817955a70df4b1989a898b01837345d0bf63
Sergei Shneider <50211129+SShneider@users.noreply.github.com>
2020-09-12 17:38:01 -0400

    Update courseraListVideosAndDuration.js

--- courseraListVideosAndDuration.js
+++ courseraListVideosAndDuration.js
@@ -4,10 +4,7 @@ let tasks = document.getElementsByClassName("rc-WeekSingleItemDisplay")
 let outstring = ''
 for(let i = 0; i<tasks.length; i++){
   let currentTask = tasks[i].getElementsByClassName("rc-WeekItemName")[0].innerText
-  console.log(i)
-  console.log(currentTask)
   currentTask = currentTask.split("\n")
-  console.log(currentTask)
   outstring+= currentTask[currentTask.length-1]
   let duration = tasks[i].getElementsByClassName("rc-EffortText")
   if(duration.length) duration = duration[0].getElementsByTagName("span")
